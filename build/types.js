0a3c64fb44e9197db4ac3db67c1ee883d281a3b0
natalia <lnataliaperri@gmail.com>
2023-03-22 15:22:38 -0300

    build

--- build/types.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CATEGORY = void 0;
-var CATEGORY;
-(function (CATEGORY) {
-    CATEGORY["ACCESSORIES"] = "Acess\u00F3rios";
-    CATEGORY["CLOTHES_AND_SHOES"] = "Roupas e cal\u00E7ados";
-    CATEGORY["ELECTRONICS"] = "Eletr\u00F4nicos";
-})(CATEGORY = exports.CATEGORY || (exports.CATEGORY = {}));
