26e087fa104489f6b03b2c53929dc8ee88165a71
natalia <lnataliaperri@gmail.com>
2023-03-27 15:39:19 -0300

    Endpoints criados

--- src/database.ts
+++ src/database.ts
@@ -1,149 +1,149 @@
 import { CATEGORY, TProduct, TPurchase, TUser } from './types'
 
-const users: TUser[] = []
-const products: TProduct[] = []
-const purchases: TPurchase[] = []
+export const users: TUser[] = [
+  {
+    id: 'u001',
+    email: 'natalia@gmail.com',
+    password: 'natalia123',
+  },
+  {
+    id: 'u002',
+    email: 'daniel@gmail.com',
+    password: 'daniel123',
+  },
+]
 
-function createUser(id: string, email: string, password: string): void {
-  const userVerification = users.find(
-    (user) => user.email === email || user.id === id
-  )
+export const products: TProduct[] = [
+  {
+    id: 'p001',
+    name: 't-shirt',
+    price: 49.9,
+    category: CATEGORY.CLOTHES_AND_SHOES,
+  },
+  {
+    id: 'p002',
+    name: 'necklace',
+    price: 89.9,
+    category: CATEGORY.ACCESSORIES,
+  },
+]
+export const purchases: TPurchase[] = [
+  {
+    userId: users[0].id,
+    productId: products[0].id,
+    quantity: 1,
+    totalPrice: products[0].price * 1,
+  },
+  {
+    userId: users[1].id,
+    productId: products[1].id,
+    quantity: 2,
+    totalPrice: products[1].price * 2,
+  },
+]
 
-  if (userVerification) {
-    console.log('Usuário já cadastrado')
-  } else {
-    const newUser = {
-      id,
-      email,
-      password,
-    }
+// function createUser(id: string, email: string, password: string): void {
+//   const userVerification = users.find(
+//     (user) => user.email === email || user.id === id
+//   )
 
-    users.push(newUser)
+//   if (userVerification) {
+//     console.log('Usuário já cadastrado')
+//   } else {
+//     const newUser = {
+//       id,
+//       email,
+//       password,
+//     }
 
-    console.log('Cadastro realizado com sucesso')
-  }
-}
+//     users.push(newUser)
 
-function getAllUsers(): void {
-  console.log(users)
-}
+//     console.log('Cadastro realizado com sucesso')
+//   }
+// }
 
-function createProduct(
-  id: string,
-  name: string,
-  price: number,
-  category: CATEGORY
-): void {
-  const newProduct = {
-    id,
-    name,
-    price,
-    category,
-  }
+// function getAllUsers(): void {
+//   console.log(users)
+// }
 
-  products.push(newProduct)
+// function createProduct(
+//   id: string,
+//   name: string,
+//   price: number,
+//   category: CATEGORY
+// ): void {
+//   const newProduct = {
+//     id,
+//     name,
+//     price,
+//     category,
+//   }
 
-  console.log('Produto criado com sucesso')
-}
+//   products.push(newProduct)
 
-function getAllProducts() {
-  console.log(products)
-}
+//   console.log('Produto criado com sucesso')
+// }
 
-function getProductById(idToSearch: string): void {
-  const productId = products.find(
-    (product) => product.id.toLowerCase() === idToSearch.toLowerCase()
-  )
+// function getAllProducts() {
+//   console.log(products)
+// }
 
-  if (productId) {
-    console.log(productId)
-  } else {
-    console.log('Produto não encontrado')
-  }
-}
+// function getProductById(idToSearch: string): void {
+//   const productId = products.find(
+//     (product) => product.id.toLowerCase() === idToSearch.toLowerCase()
+//   )
 
-function queryProductsByName(q: string): void {
-  const queryProducts = products.filter(
-    (product) => product.name.toLowerCase() === q.toLowerCase()
-  )
+//   if (productId) {
+//     console.log(productId)
+//   } else {
+//     console.log('Produto não encontrado')
+//   }
+// }
 
-  if (queryProducts.length !== 0) {
-    console.log(queryProducts)
-  } else {
-    console.log('Produto não encontrado')
-  }
-}
+// function queryProductsByName(q: string): void {
+//   const queryProducts = products.filter(
+//     (product) => product.name.toLowerCase() === q.toLowerCase()
+//   )
 
-function createPurchase(
-  userId: string,
-  productId: string,
-  quantity: number,
-  totalPrice: number
-): void {
-  const newPurchase = {
-    userId,
-    productId,
-    quantity,
-    totalPrice,
-  }
+//   if (queryProducts.length !== 0) {
+//     console.log(queryProducts)
+//   } else {
+//     console.log('Produto não encontrado')
+//   }
+// }
 
-  purchases.push(newPurchase)
+// function createPurchase(
+//   userId: string,
+//   productId: string,
+//   quantity: number,
+//   totalPrice: number
+// ): void {
+//   const newPurchase = {
+//     userId,
+//     productId,
+//     quantity,
+//     totalPrice,
+//   }
 
-  console.log('Compra realizada com sucesso')
-}
+//   purchases.push(newPurchase)
 
-function getAllPurchasesFromUserId(userIdToSearch: string): void {
-  const userPurchasesIds = purchases
-    .filter(
-      (purchase) =>
-        purchase.userId.toLowerCase() === userIdToSearch.toLowerCase()
-    )
-    .map((purchase) => purchase.productId)
+//   console.log('Compra realizada com sucesso')
+// }
 
-  if (userPurchasesIds.length !== 0) {
-    const userProducts = products.filter((product) =>
-      userPurchasesIds.includes(product.id)
-    )
-    console.log(userProducts)
-  } else {
-    console.log('Produto não encontrado')
-  }
-}
+// function getAllPurchasesFromUserId(userIdToSearch: string): void {
+//   const userPurchasesIds = purchases
+//     .filter(
+//       (purchase) =>
+//         purchase.userId.toLowerCase() === userIdToSearch.toLowerCase()
+//     )
+//     .map((purchase) => purchase.productId)
 
-console.log('---Create User---\n')
-createUser('1', 'lnataliaperri@gmail.com', 'natalia123')
-createUser('2', 'danielluis@gmail.com', 'dnl123')
-createUser('3', 'lnataliaperri@gmail.com', 'nat123')
-createUser('1', 'dndnd@gmail.com', 'awdas')
-
-console.log('\n---Get All Users--- \n')
-getAllUsers()
-
-console.log('\n---Create Product--- \n')
-createProduct('1', 't-shirt', 40, CATEGORY.CLOTHES_AND_SHOES)
-createProduct('2', 'necklace', 80, CATEGORY.ACCESSORIES)
-
-console.log('\n---Get All Products---\n')
-getAllProducts()
-
-console.log('\n---Get Product By Id--- \n')
-getProductById('1')
-getProductById('2')
-getProductById('3')
-
-console.log('\n---Query Products By Name---\n')
-queryProductsByName('t-shirt')
-queryProductsByName('necklace')
-queryProductsByName('blabla')
-
-console.log('\n---Create Purchase---\n')
-createPurchase(users[0].id, products[0].id, 1, products[0].price)
-createPurchase(users[1].id, products[1].id, 2, products[1].price * 2)
-
-console.log('\n---All Purchases--- \n')
-console.log(purchases)
-
-console.log('\n---Get All Purchases From User Id \n')
-getAllPurchasesFromUserId('1')
-getAllPurchasesFromUserId('2')
-getAllPurchasesFromUserId('3')
+//   if (userPurchasesIds.length !== 0) {
+//     const userProducts = products.filter((product) =>
+//       userPurchasesIds.includes(product.id)
+//     )
+//     console.log(userProducts)
+//   } else {
+//     console.log('Produto não encontrado')
+//   }
+// }
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,115 @@
+import express, { Request, Response } from 'express'
+import cors from 'cors'
+import { products, purchases, users } from './database'
+import { TProduct, TPurchase, TUser } from './types'
 
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+app.listen(3003, () => {
+  console.log('Servidor rodando na porta 3003')
+})
+
+app.get('/users', (req: Request, res: Response) => {
+  res.status(200).send(users)
+})
+
+app.get('/products', (req: Request, res: Response) => {
+  res.status(200).send(products)
+})
+
+app.get('/purchases', (req: Request, res: Response) => {
+  res.status(200).send(purchases)
+})
+
+app.get('/products/search', (req: Request, res: Response) => {
+  const query = req.query.q as string
+  const result = products.filter((product) =>
+    product.name.toLowerCase().includes(query.toLowerCase())
+  )
+
+  if (result.length === 0) {
+    return res.status(404).send('Produto não encontrado')
+  }
+
+  res.status(200).send(result)
+})
+
+app.post('/users', (req: Request, res: Response) => {
+  const body = req.body
+  const { id, email, password } = body
+
+  if (!id || !email || !password) {
+    return res.status(400).send('Error')
+  }
+
+  const idAlreadyExists = users.find((user) => user.id === id)
+
+  if (idAlreadyExists) {
+    return res.status(409).send('Id já cadastrado')
+  }
+
+  const emailAlreadyExists = users.find((user) => user.email === email)
+
+  if (emailAlreadyExists) {
+    return res.status(409).send('E-mail já cadastrado')
+  }
+
+  const newUser: TUser = {
+    id,
+    email,
+    password,
+  }
+
+  users.push(newUser)
+
+  res.status(201).send('Usuário cadastrado com sucesso')
+})
+
+app.post('/products', (req: Request, res: Response) => {
+  const body = req.body
+  const { id, name, price, category } = body
+
+  if (!id || !name || !price || !category) {
+    return res.status(400).send('Error')
+  }
+
+  const idAlreadyExists = products.find((product) => product.id === id)
+
+  if (idAlreadyExists) {
+    return res.status(409).send('Id já cadastrado')
+  }
+
+  const newProduct: TProduct = {
+    id,
+    name,
+    price,
+    category,
+  }
+
+  products.push(newProduct)
+
+  res.status(201).send('Produto cadastrado com sucesso')
+})
+
+app.post('/purchases', (req: Request, res: Response) => {
+  const body = req.body
+  const { userId, productId, quantity, totalPrice } = body
+
+  if (!userId || !productId || !quantity || !totalPrice) {
+    return res.status(400).send('Error')
+  }
+
+  const newPurchase: TPurchase = {
+    userId,
+    productId,
+    quantity,
+    totalPrice,
+  }
+
+  purchases.push(newPurchase)
+
+  res.status(201).send('Compra realizada com sucesso')
+})
